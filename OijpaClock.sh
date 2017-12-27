e5ce5b5065dce5465e4f4c5736f01352aa1dcd37
Greg Oij <goij@aurora.edu>
2017-12-26 22:24:36 -0600

    More Generic OijpaClock Sturr

--- OijpaClock.sh
+++ OijpaClock.sh
@@ -4,29 +4,22 @@
 # make a noise every hour on the hour like a clock.
 
 # Crontab Entry:
+# 0 * * * * /home/pi/OijpaClock/OijpaClock.sh > /dev/null 2>/dev/null
+
+#Crontab Entries: (if using an id other than goij, change the home directory, and obviously uncomment them!)
 # 0 * * * * /home/goij/OijpaClock/OijpaClock.sh > /dev/null 2>/dev/null
-# Play the 15 minute sound:
-#15  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock15.mp3 > /dev/null 2>/dev/null&
-# Play the 30 minute sound:
-#30  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock30.mp3 > /dev/null 2>/dev/null&
-# Play the 45 minute sound:
-#45  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock45.mp3 > /dev/null 2>/dev/null&
-# Play Tick Tock every minute
-#*  *  *  *  *  sudo /usr/bin/omxplayer /home/goij/OijpaClock/TickTock.mp3 > /dev/null 2>/dev/null&
-# Flash a 3 LED LED for about a second every minute
-#*  *  *  *  *  sudo /home/goij/bin/fl3-1&
-
-# Still need to adjust volume higher for Jack, and lower for the "bong."
-# And then back to origional volume.
-
+# 15  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock15.mp3 > /dev/null 2>/dev/null&
+# 30  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock30.mp3 > /dev/null 2>/dev/null&
+# 45  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock45.mp3 > /dev/null 2>/dev/null&
+# *  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/tick.mp3 > /dev/null 2>/dev/null&
 HOUR=`date +%H`
 
 echo $HOUR
 
-cd /home/goij/OijpaClock
+cd /home/pi/OijpaClock
 
-/home/goij/bin/fl3&
-sudo amixer set PCM 90% >/dev/null 2>/dev/null
+sudo mpg123 Westminster.mp3 > /dev/null 2>/dev/null
+sudo mpg123 $HOUR.mp3 > /dev/null 2>/dev/null
 
 case $HOUR in
 13)
@@ -64,20 +57,11 @@ case $HOUR in
 	;;
 esac
 echo $HOUR
-
-sudo amixer set PCM 95% >/dev/null 2>/dev/null
-#sudo mpg123 PDDong.mp3 > /dev/null 2>/dev/null
-sudo mpg123 $HOUR.mp3 > /dev/null 2>/dev/null
-sudo mpg123 Westminster.mp3 > /dev/null 2>/dev/null
+	
 
 HOURCOUNT="0"
 while [ $HOURCOUNT -lt $HOUR ]; do
 	sudo mpg123 ZDong6.mp3 > /dev/null 2>/dev/null
 	HOURCOUNT=`expr $HOURCOUNT + 1`
 done
-sudo amixer set PCM 90% >/dev/null 2>/dev/null
-sudo mpg123 $HOUR.mp3 > /dev/null 2>/dev/null
-sudo amixer set PCM 80% >/dev/null 2>/dev/null
-#echo "$HOUR O Clock" | sudo espeak -a 175 >/dev/null 2>/dev/null
-echo "$HOUR O Clock" | sudo espeak -p 60 -s 120 -a 175 > /dev/null 2>/dev/null
-sudo amixer set PCM 90% >/dev/null 2>/dev/null
+echo "The time is $HOUR of the clock or is it?" | sudo festival --tts
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# OijpaClock
+OijpaClock
+Create a $HOME/bin directory and link badjokes and rndjoke to it.
 Shell Script and Sounds called by Cron Jobs for a Grandpa Clock (Also bad jokes)
+
+To add assorted things to crontab, type 'crontab -e' and type:
+# m h  dom mon dow   command
+0 * * * * /home/pi/OijpaClock/OijpaClock.sh > /dev/null 2>/dev/null
+15  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock15.mp3 > /dev/null 2>/dev/null&
+30  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock30.mp3 > /dev/null 2>/dev/null&
+45  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock45.mp3 > /dev/null 2>/dev/null&
+# This one takes 3 LED's connected; the /home/pi/pybin/fl3-1.py file must be ready.
+# *  *  *  *  *  sudo /home/pi/bin/fl3-1 &
+*  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/TickTock.mp3 > /dev/null 2>/dev/null&
+# This script shoots two lasers.
+# *  *  *  *  *  /home/pi/bin/lasers1sec > /dev/null 2>/dev/null&
+# This one runs bad jokes based on a pull-up resistor that gets pulled down with a switch:
+#@reboot /home/pi/bin/runBadJoke > /dev/null 2>/dev/null&
+
+...
+You will need to add festival and espeak to your apps...
+sudo apt-get update
+sudo apt-get install festival espeak
