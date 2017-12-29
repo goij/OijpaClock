03628a77de1f6ee91408ef4e79e5b6f105aa594a
Greg Oij <goij@aurora.edu>
2017-12-29 01:15:33 -0600

    Modified bad jokes and the README.md

--- OijpaClock.sh
+++ OijpaClock.sh
@@ -4,14 +4,14 @@
 # make a noise every hour on the hour like a clock.
 
 # Crontab Entry:
-# 0 * * * * /home/pi/OijpaClock/OijpaClock.sh > /dev/null 2>/dev/null
+# 0 * * * * /home/pi/OijpaClock/OijpaClock.sh > /dev/null 2>/dev/null&
 
-#Crontab Entries: (if using an id other than goij, change the home directory, and obviously uncomment them!)
-# 0 * * * * /home/goij/OijpaClock/OijpaClock.sh > /dev/null 2>/dev/null
-# 15  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock15.mp3 > /dev/null 2>/dev/null&
-# 30  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock30.mp3 > /dev/null 2>/dev/null&
-# 45  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/ZachGrandmaClock45.mp3 > /dev/null 2>/dev/null&
-# *  *  *  *  *  sudo /usr/bin/mpg123 /home/goij/OijpaClock/tick.mp3 > /dev/null 2>/dev/null&
+#Crontab Entries: (if using an id other than pi, change the home directory, and obviously uncomment them!)
+# 0 * * * * /home/pi/OijpaClock/OijpaClock.sh > /dev/null 2>/dev/null
+# 15  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock15.mp3 > /dev/null 2>/dev/null&
+# 30  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock30.mp3 > /dev/null 2>/dev/null&
+# 45  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock45.mp3 > /dev/null 2>/dev/null&
+# *  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/TickTock.mp3 > /dev/null 2>/dev/null&
 HOUR=`date +%H`
 
 echo $HOUR
@@ -64,4 +64,4 @@ while [ $HOURCOUNT -lt $HOUR ]; do
 	sudo mpg123 ZDong6.mp3 > /dev/null 2>/dev/null
 	HOURCOUNT=`expr $HOURCOUNT + 1`
 done
-echo "The time is $HOUR of the clock or is it?" | sudo festival --tts
+echo "The time is $HOUR of the clock, or is it?" | sudo festival --tts
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 OijpaClock
+
+This script was created for raspberry pi's but can be used on any Linux or Unix system.
 Create a $HOME/bin directory and link badjokes and rndjoke to it.
 Shell Script and Sounds called by Cron Jobs for a Grandpa Clock (Also bad jokes)
 
@@ -8,9 +10,10 @@ To add assorted things to crontab, type 'crontab -e' and type:
 15  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock15.mp3 > /dev/null 2>/dev/null&
 30  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock30.mp3 > /dev/null 2>/dev/null&
 45  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/ZachGrandmaClock45.mp3 > /dev/null 2>/dev/null&
+*  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/TickTock.mp3 > /dev/null 2>/dev/null&
+
 # This one takes 3 LED's connected; the /home/pi/pybin/fl3-1.py file must be ready.
 # *  *  *  *  *  sudo /home/pi/bin/fl3-1 &
-*  *  *  *  *  sudo /usr/bin/mpg123 /home/pi/OijpaClock/TickTock.mp3 > /dev/null 2>/dev/null&
 # This script shoots two lasers.
 # *  *  *  *  *  /home/pi/bin/lasers1sec > /dev/null 2>/dev/null&
 # This one runs bad jokes based on a pull-up resistor that gets pulled down with a switch:
